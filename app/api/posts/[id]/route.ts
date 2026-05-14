56a1c4ea043d8627f5a0322257fa7a1137e5bd9f
expkisstheskygwon-maker <exp.kissthesky.gwon@gmail.com>
2026-05-14 23:42:46 +0900

    배포 오류 수정 2

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getCloudflareContext } from '@opennextjs/cloudflare';
+import { cookies } from 'next/headers';
 
 export async function GET(
   request: NextRequest,
